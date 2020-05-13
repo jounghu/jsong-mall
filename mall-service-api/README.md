6b20dc9f58d54b683eaa360df60069efe1999711
胡建松 <hujiansong@dobest.com>
2020-05-13 15:27:31 +0800

    add project structure

---
+++ mall-service-api/README.md
@@ -0,0 +1,3 @@
+## 提供给外部引用的API
+
+### mall-user-service-api
---
+++ mall-service/mall-cart-service/src/main/java/com/jsong/mall/cart/Cart.java
@@ -0,0 +1,17 @@
+package com.jsong.mall.cart;
+
+import lombok.Data;
+
+/**
+ * 2020/5/13 15:21
+ *
+ * @author hujiansong@dobest.com
+ * @since 1.8
+ */
+@Data
+public class Cart {
+
+    String userId;
+
+    String userName;
+}
